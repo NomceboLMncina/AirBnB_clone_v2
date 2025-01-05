4d51063a8b74173bbe6df5adc1dde569ca7ed8d1
NomceboLMncina <nomcebolungapearlmncina@gmail.com>
2025-01-05 07:18:42 -0800

    changes

--- models/user.py
+++ models/user.py
@@ -1,60 +1,17 @@
 #!/usr/bin/python3
-""" class User"""
+"""This module defines a class User"""
 from models.base_model import BaseModel, Base
-import os
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column
-from sqlalchemy import String
 
 
 class User(BaseModel, Base):
-    """class that represents user
-    Atrributes:
-        __tablename__: name for sql table
-        email: user email address.
-        passwd: user's password.
-        f_name: user's first name
-        l_name: user's lastname
-        places: user-pace relationship
-        reviews: user-review"""
+    """This class defines a user by various attributes"""
+    __tablename__ = "users"
 
-        __tablename__ = "users"
-    email = (
-        Column(String(128), nullable=False)
-        if os.getenv("HBNB_TYPE_STORAGE") == "db"
-        else ""
-    )
-
-    password = (
-        Column(String(128), nullable=False)
-        if os.getenv("HBNB_TYPE_STORAGE") == "db"
-        else ""
-    )
-
-    first_name = (
-        Column(String(128), nullable=True)
-        if os.getenv("HBNB_TYPE_STORAGE") == "db"
-        else ""
-    )
-
-    last_name = (
-        Column(String(128), nullable=True)
-        if os.getenv("HBNB_TYPE_STORAGE") == "db"
-        else ""
-    )
-
-    places = (
-        relationship(
-            "Place", cascade="all, delete, delete-orphan", backref="user"
-        )
-        if os.getenv("HBNB_TYPE_STORAGE") == "db"
-        else None
-    )
-
-    reviews = (
-        relationship(
-            "Review", cascade="all, delete, delete-orphan", backref="user"
-        )
-        if os.getenv("HBNB_TYPE_STORAGE") == "db"
-        else None
-    )
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship('Place', backref='user', cascade='delete')
+    reviews = relationship('Review', backref='user', cascade='delete')
